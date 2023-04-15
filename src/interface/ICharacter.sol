08921ce4f68869c9f58407ed0527686e2648ea60
Avi Misra <avinash.misra@uphold.com>
2023-04-15 18:32:18 -0400

    Regenreate Character and Item interfaces

--- src/interface/ICharacter.sol
+++ src/interface/ICharacter.sol
@@ -1,9 +1,30 @@
 pragma solidity ^0.8.10;
 
 interface ICharacter {
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
     event Equipped(address indexed _item, uint256 indexed slotID, uint256 indexed itemID);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     event Unequipped(address indexed _item, uint256 indexed slotID, uint256 indexed itemID);
-    event Transfer(address indexed _from, address indexed to, uint256 tokenID);
+
+    function approve(address to, uint256 tokenId) external;
+    function balanceOf(address owner) external view returns (uint256);
     function equip(uint256 characterID, uint8 slotID, uint256 itemID) external;
+    function getApproved(uint256 tokenId) external view returns (address);
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+    function isSlotEmpty(uint256 characterID, uint8 slotID) external view returns (bool);
+    function mint() external;
+    function name() external view returns (string memory);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function safeTransferFrom(address from, address to, uint256 tokenID) external;
+    function safeTransferFrom(address from, address to, uint256 tokenID, bytes memory data) external;
+    function setApprovalForAll(address operator, bool approved) external;
+    function slotEquipmentID(uint256 characterID, uint8 slotID) external view returns (uint256);
+    function slotEquippedBy(uint256 characterID, uint8 slotID) external view returns (address);
+    function slotName(uint8 slotID) external pure returns (string memory);
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+    function symbol() external view returns (string memory);
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function transferFrom(address from, address to, uint256 tokenID) external;
     function unequip(uint256 characterID, uint8 slotID, uint256 itemID) external;
 }
--- src/interface/IItem.sol
+++ src/interface/IItem.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.8.10;
 
-interface IItem{
-    function isEquipped(uint256 itemID) external returns (bool);
+interface IItem {
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    function approve(address to, uint256 tokenId) external;
+    function balanceOf(address owner) external view returns (uint256);
+    function equip(address character, uint256 charID, uint256 itemID) external;
+    function equippedTo(uint256 itemID) external view returns (address);
+    function getApproved(uint256 tokenId) external view returns (address);
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+    function isEquipped(uint256 itemID) external view returns (bool);
+    function mint() external;
+    function name() external view returns (string memory);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function safeTransferFrom(address from, address to, uint256 tokenID) external;
+    function safeTransferFrom(address from, address to, uint256 tokenID, bytes memory data) external;
+    function setApprovalForAll(address operator, bool approved) external;
+    function slotID() external view returns (uint8);
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+    function symbol() external view returns (string memory);
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function transferFrom(address from, address to, uint256 tokenID) external;
+    function unequip(address character, uint256 charID, uint256 itemID) external;
 }
