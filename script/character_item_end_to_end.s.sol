648d7153e42e723a77f8c4d4f001e153be5cdd25
Avi Misra <avinash.misra@uphold.com>
2023-04-15 17:15:41 -0400

    Add end to end script for equipping/unequipping an item on a character

---
+++ script/character_item_end_to_end.s.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+import "../src/Character.sol";
+import "../src/Item.sol";
+
+contract CharacterScript is Script {
+   function run() public {
+       vm.startBroadcast(vm.envUint("FORGE_ANVIL_PRIVATE_KEY"));
+       Character character = Character(vm.envAddress("CHARACTER_CONTRACT_ADDRESS"));
+       Item comm = Item(vm.envAddress("ITEM_CONTRACT_ADDRESS"));
+       
+       uint256 balance = character.balanceOf(vm.envAddress("FORGE_ANVIL_PUBLIC_KEY"));
+       console.log("Character Name: %s", character.name());
+       console.log("Character Symbol: %s", character.symbol());
+       console.log("Character Balance Of %s: %d", vm.envAddress("FORGE_ANVIL_PUBLIC_KEY"), balance);
+       if (balance < 1) {
+           character.mint();
+           console.log("Balance Of Character Post Mint %s: %d", vm.envAddress("FORGE_ANVIL_PUBLIC_KEY"), character.balanceOf(vm.envAddress("FORGE_ANVIL_PUBLIC_KEY")));
+       }
+
+
+       console.log("Owner of Character Token 0: %s", character.ownerOf(0)); 
+       console.log("Character Token URI: %s", character.tokenURI(0));
+
+       balance = comm.balanceOf(vm.envAddress("FORGE_ANVIL_PUBLIC_KEY"));
+       console.log("Item Name: %s", comm.name());
+       console.log("Item Symbol: %s", comm.symbol());
+       console.log("Item Balance Of %s: %d", vm.envAddress("FORGE_ANVIL_PUBLIC_KEY"), balance);
+       if (balance < 1) {
+           comm.mint();
+           console.log("Balance Of Item Post Mint %s: %d", vm.envAddress("FORGE_ANVIL_PUBLIC_KEY"), comm.balanceOf(vm.envAddress("FORGE_ANVIL_PUBLIC_KEY")));
+       }
+
+       console.log("Checking slot");
+
+       bool slotAvail = character.isSlotEmpty(0, 5);
+       console.log("Slot Empty: ", slotAvail);
+       
+       if (true == slotAvail) {
+            console.log("Equipping %s", comm.name());
+            comm.equip(vm.envAddress("CHARACTER_CONTRACT_ADDRESS"), 0, 0);
+       } else {
+           console.log("Unequipping %s", comm.name());
+           comm.unequip(vm.envAddress("CHARACTER_CONTRACT_ADDRESS"), 0, 0);
+       }
+
+       printEquipmentInfo(character, comm);
+
+
+       vm.stopBroadcast();
+   }
+
+   function printEquipmentInfo(Character character, Item item) public view {
+       bool slotAvail = character.isSlotEmpty(0,5);
+       console.log("Slot 5 (%s) Empty: %s", character.slotName(5), slotAvail);
+       if (slotAvail) {
+           return;
+       }
+       console.log("%s equipped to: ", item.name(), item.equippedTo(0));
+       console.log("Character %s slot equipped by: %s", character.slotName(5), character.slotEquippedBy(0, 5)); 
+   }
+}
